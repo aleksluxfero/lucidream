42ff8dde2e74f83b1ca8463f8ca5a9a990c08fb9
Alex <alex.luxfero@gmail.com>
2024-10-11 21:21:46 +0500

    git commit -m "тест"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { backButton } from "@telegram-apps/sdk-react";
 
 export default function Page() {
   const router = useRouter();
+
+  useEffect(() => {
+    backButton.show();
+    return () => {
+      backButton.hide();
+    };
+  }, []);
 
   return (
     <div>
