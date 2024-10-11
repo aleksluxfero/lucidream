1d3479575cb70704e89c302a15340d88f6120048
Alex <alex.luxfero@gmail.com>
2024-10-11 22:13:43 +0500

    git commit -m "проверка"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,45 +1,16 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {
-  isBackButtonMounted,
-  mountBackButton,
-  offBackButtonClick,
-  onBackButtonClick,
-  showBackButton,
-} from "@telegram-apps/sdk-react";
-import { useEffect, useState } from "react";
+import { BackButton } from "@/components/BackButton";
 
 export default function Page() {
   const router = useRouter();
-  const [buttonMounted, setButtonMounted] = useState(false);
-
-  useEffect(() => {
-    mountBackButton();
-    showBackButton();
-    if (isBackButtonMounted()) {
-      setButtonMounted(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log("состояние маунтинга", buttonMounted);
-    if (buttonMounted) {
-      const handleBackButton = () => {
-        console.log("button click");
-        router.push("/");
-      };
-      onBackButtonClick(handleBackButton);
-      return () => {
-        offBackButtonClick(handleBackButton);
-      };
-    }
-  }, [buttonMounted, router]);
 
   return (
     <div>
       <h1>Сны</h1>
       <Link href="/">На главную</Link>
+      <BackButton />
       <button onClick={() => router.back()}>Назад</button>
     </div>
   );
