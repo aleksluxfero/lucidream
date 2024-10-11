6421e5ea7fcbbc7c19f6540daabf0b6d0509806c
Alex <alex.luxfero@gmail.com>
2024-10-11 20:51:04 +0500

    git commit -m "last last fix back button click event"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,26 +1,10 @@
 "use client";
-import {
-  isBackButtonMounted,
-  isBackButtonSupported,
-  mountBackButton,
-  showBackButton,
-} from "@telegram-apps/sdk-react";
+import { mountBackButton, showBackButton } from "@telegram-apps/sdk-react";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    console.log(isBackButtonSupported());
-    if (isBackButtonSupported()) {
-      console.log("Монтируем кнопку");
-      mountBackButton();
-    }
-    console.log(isBackButtonMounted());
-    if (isBackButtonMounted()) {
-      console.log("Показываем кнопку");
-      showBackButton();
-    }
-  }, []);
+  mountBackButton();
+  showBackButton();
 
   return (
     <div>
