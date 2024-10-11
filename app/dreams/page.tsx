4c4089c5db07ac82b06fbdf55ae0dc6530138dcb
Alex <alex.luxfero@gmail.com>
2024-10-11 21:17:46 +0500

    git commit -m "тест"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,46 +1,16 @@
 "use client";
-import {
-  mountBackButton,
-  showBackButton,
-  onBackButtonClick,
-  isBackButtonMounted,
-} from "@telegram-apps/sdk-react";
+import {} from "@telegram-apps/sdk-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    mountBackButton();
-    showBackButton();
-    setIsMounted(isBackButtonMounted()); // Устанавливаем состояние при монтировании кнопки
-  }, []);
-
-  useEffect(() => {
-    const handleBackButton = () => {
-      router.push("/"); // Отмена стандартного поведения Telegram SDK (например, сворачивание приложения)
-    };
-
-    if (isMounted) {
-      // Обрабатываем нажатие кнопки "Назад", только если кнопка смонтирована
-      const offClick = onBackButtonClick(handleBackButton);
-
-      return () => {
-        offClick(); // Очищаем событие при размонтировании
-      };
-    }
-  }, [isMounted, router]);
 
   return (
     <div>
       <h1>Сны</h1>
       <Link href="/">На главную</Link>
-      <button onClick={() => onBackButtonClick(() => console.log("Тест"))}>
-        Я забыл
-      </button>
+      <button onClick={() => router.back()}>Назад</button>
     </div>
   );
 }
