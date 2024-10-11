94963ec170af1c427c99d28204b761cd67fb36da
Alex <alex.luxfero@gmail.com>
2024-10-11 20:42:51 +0500

    git commit -m "last last fix back button click event"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,30 +1,19 @@
 "use client";
+import {
+  isBackButtonMounted,
+  isBackButtonSupported,
+  mountBackButton,
+  showBackButton,
+} from "@telegram-apps/sdk-react";
 import Link from "next/link";
-import { backButton } from "@telegram-apps/sdk-react";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Page() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    backButton.mount();
-    backButton.show();
-    console.log(backButton.isSupported());
-    const handleButtonClick = () => {
-      if (pathname !== "/") {
-        console.log(pathname);
-        router.push("/");
-        return false;
-      }
-    };
-    backButton.onClick(handleButtonClick);
-
-    return () => {
-      backButton.offClick(handleButtonClick);
-    };
-  }, [pathname, router]);
+  if (isBackButtonSupported()) {
+    mountBackButton();
+  }
+  if (isBackButtonMounted()) {
+    showBackButton();
+  }
 
   return (
     <div>
