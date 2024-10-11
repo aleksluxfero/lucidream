599d78e3c6f66ed8060e1cc6be17f2fa697ac96f
Alex <alex.luxfero@gmail.com>
2024-10-11 22:20:05 +0500

    git commit -m "проверка"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { BackButton } from "@/components/BackButton";
+import { backButton, useSignal } from "@telegram-apps/sdk-react";
+import { useEffect } from "react";
 
 export default function Page() {
   const router = useRouter();
+  const isVisible = useSignal(backButton.isVisible);
+
+  useEffect(() => {
+    console.log("The button is", isVisible ? "visible" : "invisible");
+  }, [isVisible]);
+
+  useEffect(() => {
+    backButton.mount();
+    backButton.show();
+    return () => {
+      backButton.hide();
+    };
+  }, []);
 
   return (
     <div>
       <h1>Сны</h1>
       <Link href="/">На главную</Link>
-      <BackButton />
       <button onClick={() => router.back()}>Назад</button>
     </div>
   );
