0a161bf42f5cbe4fffabcccd7d210329fbd69fac
Alex <alex.luxfero@gmail.com>
2024-10-11 18:54:59 +0500

    git commit -m "back button click event"

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,7 +1,32 @@
 "use client";
 import Link from "next/link";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const handleBackButton = (event: PopStateEvent) => {
+      // Если пользователь НЕ на главной странице
+      if (pathname !== "/") {
+        event.preventDefault(); // Отменяем стандартное поведение
+        router.push("/"); // Перенаправляем на главную страницу
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("popstate", handleBackButton);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("popstate", handleBackButton);
+      }
+    };
+  }, [pathname, router]);
+
   return (
     <div>
       <h1>Сны</h1>
